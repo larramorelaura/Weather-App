4fb959f5276d57ada9ea08de498439145ab40b2a
larramorelaura <may.lauraa@gmail.com>
2023-03-13 23:06:05 -0500

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export function geoCodeData(city, apiKey){
-    return axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city},US&limit=1&appid=${apiKey}`)
+    return axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city},US&limit=1&appid=${apiKey}`)
 }
 
 export function getWeatherData(lat, lon, apiKey){
-    return axios.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}&units=imperial`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}&units=imperial`)
 }
 
 export function getHourlyWeatherData(lat, lon, apiKey){
-    return axios.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily,minutely&appid=${apiKey}&units=imperial`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily,minutely&appid=${apiKey}&units=imperial`)
 }
